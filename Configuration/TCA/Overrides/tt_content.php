ccfc17a17d5c7df0b7a1abe37123e00ff31182c2
Johannes C. Schulz <info@enzephalon.de>
2017-05-15 14:41:12 +0200

    registerModule back to localconf?

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -31,19 +31,4 @@ if (TYPO3_MODE === 'BE') {
         'CType',
         'formhandler'
     );
-
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'Typoheads.' . 'formhandler',
-        'web',
-        'log',
-        'bottom',
-        [
-            'Module' => 'index, view, selectFields, export, deleteLogRows'
-        ],
-        [
-            'access' => 'user,group',
-            'icon' => 'EXT:formhandler/Resources/Public/Icons/moduleicon.gif',
-            'labels' => 'LLL:EXT:formhandler/Resources/Private/Language/locallang_mod.xml'
-        ]
-    );
 }
